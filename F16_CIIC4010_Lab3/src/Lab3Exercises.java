93a59152b1ef2aa07fa3e8651c843bed4432a0ff
4015b6 <4015b6@coco.ece.uprm.edu>
2016-08-31 11:10:44 -0400

    Changed Frame name

--- F16_CIIC4010_Lab3/src/Lab3Exercises.java
+++ F16_CIIC4010_Lab3/src/Lab3Exercises.java
@@ -1,11 +1,11 @@
 import javax.swing.JFrame;
 
 public class Lab3Exercises {
-	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Hello, world!");
-		//myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(200, 200);
-		myFrame.setSize(400, 100);
-		myFrame.setVisible(true);
-	}
+    public static void main(String[] args) {
+        JFrame myFrame = new JFrame("Victor J. Fermin");
+        myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        myFrame.setLocation(850, 400);
+        myFrame.setSize(200, 200);
+        myFrame.setVisible(true);
+    }
 }
